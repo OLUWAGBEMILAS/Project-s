3280c04c9dec087bd15f9bb44434d36ea76d5d60
Ubuntu <ubuntu@ip-172-31-11-197.ca-central-1.compute.internal>
2023-03-24 01:03:21 +0000

    created for bash

---
+++ Instructions.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#OWNER:OLUWAGBEMILA OLUWASEYIFUNMI
+#DATE:MARCH 23 202
+#DESCRIPTION:Introduction into automation using bash shell scripting 
+
+#this command shows who you are 
+whoami
+
+
+#greetings from the machine
+echo "i hope you are doing well"
+
+
+#This command reveals the present working directorypwd
+pwd
+
+
